437f3a34cf5f7eb046d277c2aa36f1d4da3022b4
Chris Franklin <chris@cwfranklin.com>
2013-11-16 22:37:24 +0000

    Added schools controller with comments and updated csproj

---
+++ EnezaApi/Controllers/SchoolsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EnezaApi.Controllers
+{
+    public class SchoolsController : ApiController
+    {
+        [HttpGet]
+        public Object Get()
+        {
+            return "return all Schools";
+        }
+    }
+}
