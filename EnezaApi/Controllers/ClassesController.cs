bcbd4061184ac20885cc8c22f137e27102bbf7b0
Chris Franklin <chris@cwfranklin.com>
2013-11-16 22:33:14 +0000

    Added classes controller w/ comments

---
+++ EnezaApi/Controllers/ClassesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EnezaApi.Controllers
+{
+    public class ClassesController : ApiController
+    {
+        [HttpGet]
+        public Object Get()
+        {
+            return "return list of all classes";
+        }
+
+        [HttpGet]
+        public Object Students(int id)
+        {
+            return "return list of students in class";
+        }
+    }
+}
