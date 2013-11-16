dbc787a670c32165148d075cacada9c890eed7d7
Brandon Scott <brandon.scott03@gmail.com>
2013-11-16 19:24:34 +0000

    Adds a new method that returns all messages sent from the service.

--- TwilioService/TwilioService/TwilioService/Program.cs
+++ TwilioService/TwilioService/TwilioService/Program.cs
@@ -11,17 +11,21 @@ namespace TwilioService
     class Program
     {
         static Timer poll = new Timer();
-        static string AccountSid = "AC3b8cd1e12a5b6ff4e4ea25f431833a97";
-        static string AuthToken = "22964db1d502e5069c2d44c9a49c8b6c";
-        static TwilioRestClient twilio = new TwilioRestClient(AccountSid, AuthToken);
+        static string AccountSid;
+        static string AuthToken;
+        static TwilioRestClient twilio ;
         static int i = 0;
         static void Main(string[] args)
         {
 
-            var smss = twilio.ListMessages();
-            var s =  smss.Messages.Where(x => x.To == "+441332402803").FirstOrDefault();
+            //BS: Setup account details and get our client
+            AccountSid = "AC3b8cd1e12a5b6ff4e4ea25f431833a97";
+            AuthToken = "22964db1d502e5069c2d44c9a49c8b6c";
+            twilio = new TwilioRestClient(AccountSid, AuthToken);
+                       
+            //Message s =  smss.Messages.Where(x => x.To == "+441332402803").FirstOrDefault();
             
-                Console.Write(s.Body);
+                //Console.Write(s.Body);
         
 
           //  Console.WriteLine(smss.SMSMessages.First<SMSMessage>().Body);
@@ -46,7 +50,17 @@ namespace TwilioService
             //Console.WriteLine(i);
             //Console.Write(test);
         }
-            
+        /// <summary>
+        /// Returns all messages sent to the service
+        /// </summary>
+        /// <returns>Messages List</returns>
+        static List<Message> GetReceivedMessages ()  
+        {
+            var smss = twilio.ListMessages();
+            List<Message> messages = smss.Messages.Where(x => x.To == "+441332402803").ToList();
+            return messages;
+          ;
+        } 
 
     }
  }
