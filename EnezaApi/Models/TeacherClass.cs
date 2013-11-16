a85e76be9540e958b1d62cceecb8652842866e7d
Brandon Scott <brandon.scott03@gmail.com>
2013-11-16 22:29:15 +0000

    Added TeacherClass model

---
+++ EnezaApi/Models/TeacherClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EnezaApi.Models
+{
+    public class TeacherClass
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int32 teacher_class_id { get; set; }
+        [ForeignKey("User"), Column(Order = 0)]
+        public Int32 teacher { get; set; }
+        [ForeignKey("Class"), Column(Order = 0)]
+        public Int32 @class { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Class @Class { get; set; }
+    }
+}
