dd272222d55783ff96c9b67dadbd159a2829e9ef
Brandon Scott <brandon.scott03@gmail.com>
2013-11-16 22:36:10 +0000

    Implemented MessageType model

---
+++ EnezaApi/Models/MessageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EnezaApi.Models
+{
+    public class MessageType
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int32 message_type_id { get; set; }
+        public String name { get; set; }
+    }
+}
