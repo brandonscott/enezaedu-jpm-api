3cd632f8d587adc3fd0c4c6dcb5510f64b8eb80d
Chris Franklin <chris@cwfranklin.com>
2013-11-16 14:43:39 +0000

    Added base web API

---
+++ EnezaApi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EnezaApi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
