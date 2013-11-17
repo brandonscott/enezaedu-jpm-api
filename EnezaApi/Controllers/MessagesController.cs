0ffb97fea04d6880524e9de16aa4d66d515cf940
Chris Franklin <chris@cwfranklin.com>
2013-11-17 05:00:58 +0000

    Changed args

--- EnezaApi/Controllers/MessagesController.cs
+++ EnezaApi/Controllers/MessagesController.cs
@@ -14,15 +14,19 @@ namespace EnezaApi.Controllers
     public class MessagesController : ApiController
     {
         [HttpPost]
-        public Object Post(String toNumber, String fromNumber, Int32 sent_time, Int32 received_time, String body, String message_type)
+        public Object Post(HttpRequestMessage request)
+            
+            //String toNumber, String fromNumber, Int32 sent_time, Int32 received_time, String body, String message_type)
         {
+            JObject postData = JObject.Parse(request.Content.ReadAsStringAsync().Result);
+
             Message message = new Message();
-            message.to_user = Models.User.GetIdFromNumber(toNumber);
-            message.from_user = Models.User.GetIdFromNumber(fromNumber);
-            message.sent_time = sent_time;
-            message.received_time = received_time;
-            message.body = body;
-            message.message_type = MessageType.GetIdByName(message_type);
+            message.to_user = Models.User.GetIdFromNumber((String)postData["toNumber"]);
+            message.from_user = Models.User.GetIdFromNumber((String)postData["fromNumber"]);
+            message.sent_time = (Int32)postData["sent_time"];
+            message.received_time = (Int32)postData["received_time"];
+            message.body = (String)postData["body"];
+            message.message_type = MessageType.GetIdByName((String)postData["message_time"]);
 
             if (message.to_user == 0 || message.from_user == 0)
             {
