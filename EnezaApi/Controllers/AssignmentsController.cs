b991a49f7d623e4f156142c15d76688bc1b07c54
Chris Franklin <chris@cwfranklin.com>
2013-11-17 10:33:13 +0000

    Added assignments controller and added model methods

---
+++ EnezaApi/Controllers/AssignmentsController.cs
@@ -0,0 +1,27 @@
+﻿using EnezaApi.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EnezaApi.Controllers
+{
+    public class AssignmentsController : ApiController
+    {
+        [HttpGet]
+        public Object Get()
+        {
+            List<Assignment> assignments = Assignment.GetAll();
+            return JObject.FromObject(new
+            {
+                assignments = assignments.Select(a =>
+                {
+                    return Assignment.OutputObject(a);
+                })
+            });
+        }
+    }
+}
