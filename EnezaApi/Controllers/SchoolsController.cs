e2033545c7e17ce4003086c1eb3bbd9d69ea63c8
Chris Franklin <chris@cwfranklin.com>
2013-11-17 07:21:34 +0000

    Added hard-coded report for school scores

--- EnezaApi/Controllers/SchoolsController.cs
+++ EnezaApi/Controllers/SchoolsController.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using Newtonsoft.Json.Linq;
+using System.Collections;
+
 namespace EnezaApi.Controllers
 {
     public class SchoolsController : ApiController
@@ -24,6 +27,17 @@ namespace EnezaApi.Controllers
                 })
             });
             //return "return all Schools";
+        }
+
+        [HttpGet]
+        public Object AverageScores()
+        {
+            return JObject.FromObject(new { 
+                schools = new ArrayList {
+                    new { score = 87.6, name = "Bob's" },
+                    new { score = 95.6, name = "Chris'" }
+                }
+            });
         }
     }
 }
